d90b542863f2a999a6b6239e0ac431701a93efdc
Zi-web <37513694+Zi-web@users.noreply.github.com>
2021-02-26 11:44:41 +0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+const burger = document.querySelector( '.burger' ),
+      nav = document.querySelector( '.nav-links' ),
+      links = document.querySelectorAll( '.nav-links li' );
+
+ function slider () {
+  burger.addEventListener( 'click', () => {
+    
+    nav.classList.toggle( 'nav-active' );
+    
+    burger.classList.toggle( 'toggle' );
+    
+    links.forEach(( link, index ) => {
+      if( link.style.animation ) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinksFade .5s ease forwards ${index/7 + 0.3}s`;
+      }
+    });
+  });
+}
+
+slider();
---
+++ js/slick-slider.js
@@ -0,0 +1,12 @@
+$().ready(function(){
+  $('.slick-carousel').slick({
+    arrows: true,
+    centerPadding: "0px",
+    dots: true,
+    slidesToShow: 1,
+    infinite: true
+   
+    //prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-up"></i></button>',
+    //nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-down"></i></button>',
+  });
+});
